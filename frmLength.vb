e47ac02972abfd579fbf7b1e594f44c79e2be867
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-03-11 10:08:04 +0200

    Add files via upload

---
+++ frmLength.vb
@@ -0,0 +1,14 @@
+﻿Public Class frmLength
+
+  Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+    Dim totalInches As Integer
+    Dim feet As Integer
+    Dim inches As Integer
+    totalInches = 41
+    feet = totalInches \ 12
+    inches = totalInches Mod 12
+    lstResults.Items.Add(feet)
+    lstResults.Items.Add(inches)
+  End Sub
+
+End Class
